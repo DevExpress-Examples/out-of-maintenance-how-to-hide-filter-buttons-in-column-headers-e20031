bd5fe2c48c50eaa74a4a6722666e277ec45c7280
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/2/Default MVVM/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DXWpfApplication;
+using DevExpress.Xpf.Grid;
+using System.Windows.Markup;
+
+namespace Default_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TestDataViewsModel(); 
+        }
+    }
+
+    public class FilterButtonvisibilityConverter : MarkupExtension, IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string fieldName = value.ToString();
+            return fieldName == "Text";
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VB/Default MVVM/MainWindow.xaml.vb
@@ -0,0 +1,48 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DXWpfApplication
+Imports DevExpress.Xpf.Grid
+Imports System.Windows.Markup
+
+Namespace Default_MVVM
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			DataContext = New TestDataViewsModel()
+		End Sub
+	End Class
+
+	Public Class FilterButtonvisibilityConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+			Dim fieldName As String = value.ToString()
+			Return fieldName = "Text"
+		End Function
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
+	End Class
+End Namespace
